4114b1f42c103f7f2edf313b1fb22d78edef563b
Srivasu-U <suvasista2@gmail.com>
2024-10-19 22:46:00 +0530

    Centralizing shared types for structs and interfaces

---
+++ types/types.go
@@ -0,0 +1,27 @@
+package types
+
+import "time"
+
+type RegisterUserPayload struct {
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+	Email     string `json:"email"`
+	Password  string `json:"password"`
+}
+
+type User struct {
+	ID        int       `json:"id"`
+	FirstName string    `json:"firstName"`
+	LastName  string    `json:"lastName"`
+	Email     string    `json:"email"`
+	Password  string    `json:"-"`
+	CreatedAt time.Time `json:"createdAt"`
+}
+
+type UserStore interface {
+	// The Store struct from store.go basically implements this interface, ie, Store struct can wrap up into UserStore interface.
+	// This will help in testing
+	GetUserByEmail(email string) (*User, error)
+	GetUserByID(id int) (*User, error)
+	CreateUser(User) error
+}
