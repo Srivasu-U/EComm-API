473bb5781de57688692b2711769fcb099b8fc0a0
Srivasu-U <suvasista2@gmail.com>
2024-10-20 23:47:58 +0530

    JWT auth support

--- config/env.go
+++ config/env.go
@@ -3,6 +3,7 @@ package config
 import (
 	"fmt"
 	"os"
+	"strconv"
 
 	"github.com/joho/godotenv"
 )
@@ -12,10 +13,12 @@ type Config struct {
 	PublicHost string
 	Port       string
 
-	DBUser     string
-	DBPassword string
-	DBAddress  string
-	DBName     string
+	DBUser                 string
+	DBPassword             string
+	DBAddress              string
+	DBName                 string
+	JWTExpirationInSeconds int64
+	JWTSecret              string
 }
 
 // Envs contains the initial configuration values for connecting to the database
@@ -25,18 +28,33 @@ func initConfig() Config {
 	godotenv.Load() // This picks up whatever values you have in the .env file. NEVER COMMIT THE .env FILE, as mentioned in .gitignore. This has sensitive data
 
 	return Config{
-		PublicHost: getEnv("PUBLIC_HOST", "https://localhost"),
-		Port:       getEnv("PORT", "8080"),
-		DBUser:     getEnv("DB_USER", "root"),
-		DBPassword: getEnv("DB_PASSWORD", "mypassword"),
-		DBAddress:  fmt.Sprintf("%s:%s", getEnv("DB_HOST", "127.0.0.1"), getEnv("DB_PORT", "3306")),
-		DBName:     getEnv("DB_NAME", "EComm"),
+		PublicHost:             getEnv("PUBLIC_HOST", "https://localhost"),
+		Port:                   getEnv("PORT", "8080"),
+		DBUser:                 getEnv("DB_USER", "root"),
+		DBPassword:             getEnv("DB_PASSWORD", "mypassword"),
+		DBAddress:              fmt.Sprintf("%s:%s", getEnv("DB_HOST", "127.0.0.1"), getEnv("DB_PORT", "3306")),
+		DBName:                 getEnv("DB_NAME", "test"),
+		JWTExpirationInSeconds: getEnvAsInt("JWT_EXP", 3600*24*7),
+		JWTSecret:              getEnv("JWT_SECRET", "not-so-secret"),
 	}
 }
 
-func getEnv(key, fallback string) string {
+func getEnv(key string, fallback string) string {
 	if value, ok := os.LookupEnv(key); ok {
 		return value
+	}
+
+	return fallback
+}
+
+func getEnvAsInt(key string, fallback int64) int64 {
+	if value, ok := os.LookupEnv(key); ok {
+		i, err := strconv.ParseInt(value, 0, 64)
+		if err != nil {
+			return fallback
+		}
+
+		return i
 	}
 
 	return fallback
--- go.mod
+++ go.mod
@@ -23,6 +23,7 @@ require (
 require (
 	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/go-playground/validator/v10 v10.22.1
+	github.com/golang-jwt/jwt/v5 v5.2.1
 	github.com/golang-migrate/migrate v3.5.4+incompatible
 	github.com/golang-migrate/migrate/v4 v4.18.1
 	github.com/joho/godotenv v1.5.1
