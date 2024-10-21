5b2fc7794524b1a85373ef59ba78b3ec3bbf93b3
Srivasu-U <suvasista2@gmail.com>
2024-10-21 14:05:01 +0530

    Getting products by ID method

--- service/product/store.go
+++ service/product/store.go
@@ -2,6 +2,8 @@ package product
 
 import (
 	"database/sql"
+	"fmt"
+	"strings"
 
 	"github.com/Srivasu-U/EComm-API/types"
 )
@@ -31,6 +33,44 @@ func (s *Store) GetProducts() ([]types.Product, error) {
 	}
 
 	return products, nil
+}
+
+func (s *Store) GetProductsByIDs(productIds []int) ([]types.Product, error) {
+	placeholders := strings.Repeat(",?", len(productIds)-1)
+	query := fmt.Sprintf("SELECT * FROM products where id in (?%s)", placeholders)
+
+	// Convert to interface{}
+	args := make([]interface{}, len(productIds))
+	for i, v := range productIds {
+		args[i] = v
+	}
+
+	rows, err := s.db.Query(query, args...)
+	if err != nil {
+		return nil, err
+	}
+
+	products := []types.Product{}
+	for rows.Next() {
+		p, err := scanRowsIntoProduct(rows)
+		if err != nil {
+			return nil, err
+		}
+
+		products = append(products, *p)
+	}
+
+	return products, nil
+}
+
+func (s *Store) UpdateProduct(product types.Product) error {
+	_, err := s.db.Exec("UPDATE products SET name = ?, price = ?, image = ?, description = ?, quantity = ? WHERE id = ?",
+		product.Name, product.Price, product.Image, product.Description, product.Quantity, product.ID)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func scanRowsIntoProduct(rows *sql.Rows) (*types.Product, error) {
