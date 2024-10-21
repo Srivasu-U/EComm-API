41ffae18d47464f51ccf5ef60adecd6db7e96494
Srivasu-U <suvasista2@gmail.com>
2024-10-21 14:30:38 +0530

    JWT Auth mechanisms

--- service/auth/jwt.go
+++ service/auth/jwt.go
@@ -1,12 +1,22 @@
 package auth
 
 import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
 	"strconv"
 	"time"
 
 	"github.com/Srivasu-U/EComm-API/config"
+	"github.com/Srivasu-U/EComm-API/types"
+	"github.com/Srivasu-U/EComm-API/utils"
 	"github.com/golang-jwt/jwt/v5"
 )
+
+type contextKey string
+
+const UserKey contextKey = "userId"
 
 func CreateJwt(secret []byte, userId int) (string, error) {
 	expiration := time.Second * time.Duration(config.Envs.JWTExpirationInSeconds)
@@ -22,4 +32,72 @@ func CreateJwt(secret []byte, userId int) (string, error) {
 	}
 
 	return tokenString, nil
+}
+
+func WithJWTAuth(handlerFunc http.HandlerFunc, store types.UserStore) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		tokenString := getTokenFromRequest(r)
+
+		token, err := validateToken(tokenString)
+		if err != nil {
+			log.Printf("failed to validate token: %v", err)
+			permissionDenied(w)
+			return
+		}
+
+		if !token.Valid {
+			log.Print("invalid token", err)
+			permissionDenied(w)
+			return
+		}
+
+		claims := token.Claims.(jwt.MapClaims)
+		str := claims["userId"].(string)
+
+		userId, _ := strconv.Atoi(str)
+		u, err := store.GetUserByID(userId)
+		if err != nil {
+			log.Printf("failed to get user by id: %v", err)
+			permissionDenied(w)
+			return
+		}
+
+		ctx := r.Context()
+		ctx = context.WithValue(ctx, UserKey, u.ID)
+		r = r.WithContext(ctx)
+
+		handlerFunc(w, r)
+	}
+}
+
+func getTokenFromRequest(r *http.Request) string {
+	tokenAuth := r.Header.Get("Authorization")
+
+	if tokenAuth != "" {
+		return tokenAuth
+	}
+
+	return ""
+}
+
+func validateToken(t string) (*jwt.Token, error) {
+	return jwt.Parse(t, func(t *jwt.Token) (interface{}, error) {
+		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", t.Header["alg"])
+		}
+		return []byte(config.Envs.JWTSecret), nil
+	})
+}
+
+func permissionDenied(w http.ResponseWriter) {
+	utils.WriteError(w, http.StatusForbidden, fmt.Errorf("Permission denied"))
+}
+
+func GetUserIDFromContext(ctx context.Context) int {
+	userId, ok := ctx.Value(UserKey).(int)
+	if !ok {
+		return -1
+	}
+
+	return userId
 }
