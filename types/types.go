6a7e45f187112fbe60ea35b956477cc04566c7bf
Srivasu-U <suvasista2@gmail.com>
2024-10-21 14:04:32 +0530

    Adding cart and order types

--- types/types.go
+++ types/types.go
@@ -14,6 +14,15 @@ type LoginUserPayload struct {
 	Password string `json:"password" validate:"required"`
 }
 
+type CartCheckoutPayload struct {
+	Items []CartItem `json:"items" validate:"required"`
+}
+
+type CartItem struct {
+	ProductID int `json:"productId"`
+	Quantity  int `json:"quantity"`
+}
+
 type User struct {
 	ID        int       `json:"id"`
 	FirstName string    `json:"firstName"`
@@ -33,6 +42,24 @@ type Product struct {
 	CreatedAt   time.Time `json:"createdAt"`
 }
 
+type Order struct {
+	ID        int       `json:"id"`
+	UserID    int       `json:"userId"`
+	Total     float64   `json:"total"`
+	Status    string    `json:"status"`
+	Address   string    `json:"address"`
+	CreatedAt time.Time `json:"createdAt"`
+}
+
+type OrderItem struct {
+	ID        int       `json:"id"`
+	OrderID   int       `json:"orderId"`
+	ProductID int       `json:"productId"`
+	Quantity  int       `json:"quantity"`
+	Price     float64   `json:"price"`
+	CreatedAt time.Time `json:"createdAt"`
+}
+
 type UserStore interface {
 	// The Store struct from store.go basically implements this interface, ie, Store struct can wrap up into UserStore interface.
 	// This will help in testing
@@ -43,4 +70,11 @@ type UserStore interface {
 
 type ProductStore interface {
 	GetProducts() ([]Product, error)
+	GetProductsByIDs(ps []int) ([]Product, error)
+	UpdateProduct(Product) error
+}
+
+type OrderStore interface {
+	CreateOrder(Order) (int, error)
+	CreateOrderItem(OrderItem) error
 }
