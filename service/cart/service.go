34ff1b7904c16463597d900d678040de9da637a0
Srivasu-U <suvasista2@gmail.com>
2024-10-21 14:46:07 +0530

    Fixing problem with error propagation. Was sending nil, now correctly propagating the error

--- service/cart/service.go
+++ service/cart/service.go
@@ -28,7 +28,7 @@ func (h *Handler) createOrder(ps []types.Product, items []types.CartItem, userId
 
 	// Check if products are in stock
 	if err := checkIfCartIsInStock(items, productMap); err != nil {
-		return 0, 0, nil
+		return 0, 0, err
 	}
 
 	// Calculate total price
